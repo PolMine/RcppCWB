9eada6fe7e6fb8ce0ed349f169cee4a545303e0c
Andreas Blätte <andreas.blaette@uni-due.de>
2018-01-03 22:47:53 +0100

    moving to travis

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,9 +1,9 @@
 if( !file.exists("../windows/libcl-master/include/cl.h") ){
   
-  # if(getRversion() < "3.3.0") setInternet2()
+  if (getRversion() < "3.3.0") setInternet2()
   download.file(
-    "http://github.com/PolMine/libcl/archive/master.zip",
-    "libcl.zip", quiet = TRUE
+    "https://github.com/PolMine/libcl/archive/master.zip",
+    "libcl.zip", method = "wininet", quiet = TRUE
     )
   dir.create("../windows", showWarnings = FALSE)
   unzip("libcl.zip", exdir = "../windows")
