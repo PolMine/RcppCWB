a74b333e8796e7d2108f0715baf556f3bdbf3866
Andreas Blätte <andreas.blaette@uni-due.de>
2018-01-16 22:14:58 +0100

    portable configuration

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,16 @@
 .onLoad <- function (libname, pkgname) {
-  # adjust dataDir, if it has not yet been set
-  cwbTmpDir <- file.path(libname, pkgname, "extdata", "cwb")
-  if (.Platform$OS.type == "windows") cwbTmpDir <- gsub("^[A-Z]?:?(.*)$", "\\1", cwbTmpDir)
-  
-  reutersTmpRegistry <- file.path(cwbTmpDir, "registry", "reuters")
-  reutersTmpDataDir <- file.path(cwbTmpDir, "indexed_corpora", "reuters")
-  # checking whether the registry file exists is necessary to circumvent a 
-  # devtools::document-problem
-  .adjustRegistry(registryFile = reutersTmpRegistry, dataDir = reutersTmpDataDir)
+  # adjust dataDir in the registry file, if it has not yet been set
+  # applies only Windows, on Linux/Mac the directory may be 
+  # write protected
+  cwbDir <- file.path(libname, pkgname, "extdata", "cwb")
+  if (.Platform$OS.type == "windows"){
+    cwbDir <- gsub("^[A-Z]?:?(.*)$", "\\1", cwbDir) # volume needs to be removed
+    reutersRegistry <- file.path(cwbDir, "registry", "reuters")
+    if (!file.exists(reutersRegistry)){
+      # hiddenRegistryDir <- file.path(cwbDir, "registry", ".reuters")
+      # file.rename(from = hiddenRegistryDir, to = reutersRegistry)
+      reutersDataDir <- file.path(cwbDir, "indexed_corpora", "reuters")
+      .adjustRegistry(registryFile = reutersRegistry, dataDir = reutersDataDir)
+    }
+  }
 }
