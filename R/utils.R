4731f4d1e026930d19494846d723e59090fb0d9f
Andreas Blätte <andreas.blaette@uni-due.de>
2018-01-16 23:33:55 +0100

    towards stability on windows

--- R/utils.R
+++ R/utils.R
@@ -2,12 +2,9 @@
   if (file.exists(registryFile)){
     registry <- readLines(registryFile)
     home_position <- grep("^HOME.*?$", registry)
-    home <- gsub( "^HOME\\s+(.*?)\\s*$", "\\1", registry[home_position], perl = TRUE)
-    if (home != dataDir){
-      registry[home_position] <- paste("HOME", dataDir, sep = " ")
-      info_position <- grep("^INFO.*?$", registry)
-      registry[info_position] <- paste("INFO", file.path(dataDir, "info.md"), sep = " ")
-      writeLines(registry, con = registryFile, sep = "\n")
-    }
+    registry[home_position] <- paste("HOME", dataDir, sep = " ")
+    info_position <- grep("^INFO.*?$", registry)
+    registry[info_position] <- paste("INFO", file.path(dataDir, "info.md"), sep = " ")
+    writeLines(registry, con = registryFile, sep = "\n")
   }
 }
--- R/zzz.R
+++ R/zzz.R
@@ -2,11 +2,10 @@
   # adjust dataDir in the registry file, if it has not yet been set
   # applies only Windows, on Linux/Mac the directory may be 
   # write protected
-  cwbDir <- file.path(libname, pkgname, "extdata", "cwb")
   if (.Platform$OS.type == "windows"){
-    cwbDir <- gsub("^[A-Z]?:?(.*)$", "\\1", cwbDir) # volume needs to be removed
-    reutersRegistry <- file.path(cwbDir, "registry", "reuters")
-    reutersDataDir <- file.path(cwbDir, "indexed_corpora", "reuters")
+    reutersRegistry <- file.path(libname, pkgname, "extdata", "cwb", "registry", "reuters")
+    reutersDataDir <- file.path(libname, pkgname, "extdata", "cwb", "indexed_corpora", "reuters")
+    reutersDataDir <- gsub("^[A-Z]?:?(.*)$", "\\1", reutersDataDir) # volume needs to be removed
     if (gsub("^HOME\\s+(.*?)$", "\\1", readLines(reutersRegistry)[10]) != reutersDataDir){
       .adjustRegistry(registryFile = reutersRegistry, dataDir = reutersDataDir)
     }
