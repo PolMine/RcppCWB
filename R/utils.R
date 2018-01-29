93224c3ac91b504923a23d007807a2ef32742906
Andreas Blätte <andreas.blaette@uni-due.de>
2018-01-29 21:21:21 +0100

    using configure.win

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,10 @@
-.adjustRegistry <- function(registryFile, dataDir){
-  if (file.exists(registryFile)){
-    registry <- readLines(registryFile)
-    home_position <- grep("^HOME.*?$", registry)
-    registry[home_position] <- paste("HOME", dataDir, sep = " ")
-    info_position <- grep("^INFO.*?$", registry)
-    registry[info_position] <- paste("INFO", file.path(dataDir, "info.md"), sep = " ")
-    writeLines(registry, con = registryFile, sep = "\n")
-  }
-}
+# .adjustRegistry <- function(registryFile, dataDir){
+#   if (file.exists(registryFile)){
+#     registry <- readLines(registryFile)
+#     home_position <- grep("^HOME.*?$", registry)
+#     registry[home_position] <- paste("HOME", dataDir, sep = " ")
+#     info_position <- grep("^INFO.*?$", registry)
+#     registry[info_position] <- paste("INFO", file.path(dataDir, "info.md"), sep = " ")
+#     writeLines(registry, con = registryFile, sep = "\n")
+#   }
+# }
