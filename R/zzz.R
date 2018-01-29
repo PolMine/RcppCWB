5b93ed5947fe9563ee174bb5384f7f6a5850526c
Andreas Blätte <andreas.blaette@uni-due.de>
2018-01-29 22:26:29 +0100

    zzz

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,10 @@
     if (file.exists(registryFile)){
       registry <- readLines(registryFile)
       libDir <- gsub("^[A-Z]?:?(.*)$", "\\1", file.path(libname, pkgname))
+      print(libname)
+      print(libDir)
       homeDir <- file.path(libDir, "extdata", "cwb", "indexed_corpora", "reuters")
+      print(homeDir)
       registry[10] <- paste("HOME", homeDir, sep = " ")
       writeLines(text = registry, con = registryFile)
     }
