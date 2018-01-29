a9ff21a9b64989358b9d5115b44831c478dcb143
Andreas Blätte <andreas.blaette@uni-due.de>
2018-01-29 22:07:20 +0100

    zzz again

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onLoad <- function (libname, pkgname) {
+  if (.Platform$OS.type == "windows"){
+    registryFile <- file.path(libname, pkgname, "inst", "extdata", "cwb", "registry", "reuters")
+    registry <- readLines(registryFile)
+    libDir <- gsub("^[A-Z]?:?(.*)$", "\\1", file.path(libname, pkgname))
+    homeDir <- file.path(libDir, "extdata", "cwb", "indexed_corpora", "reuters")
+    registry[10] <- paste("HOME", homeDir, sep = " ")
+    writeLines(text = registry, con = registryFile)
+  }
+}
