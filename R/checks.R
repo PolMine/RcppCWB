45629e20a8d6e7489e4e09bc66c505f472e64777
Andreas Blätte <andreas.blaette@uni-due.de>
2018-03-11 11:36:42 +0100

    example for s_attribute_decode (Rcpp implementation) removed

--- R/checks.R
+++ R/checks.R
@@ -91,7 +91,7 @@
     if (interactive()){
       msg <- c("The drive of your current working directory differs from the drive of",
                "the registry directory. The corpus data cannot be processed by CWB tools",
-               "unless the drive is set to", regsitry_drive, "- please confirm if you",
+               "unless the drive is set to", registry_drive, "- please confirm if you",
                "agree to resetting the working directory [Y/n]:"
       )
       user_feedback <- readline(prompt = paste0(msg, collapse = " "))
