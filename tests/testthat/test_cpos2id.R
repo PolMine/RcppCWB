48283fc75284a39ab1e86b5a8c6dd3e4fc8fc104
Andreas Blätte <andreas.blaette@uni-due.de>
2019-01-08 23:19:35 +0100

    test adjusted

--- tests/testthat/test_cpos2id.R
+++ tests/testthat/test_cpos2id.R
@@ -5,10 +5,11 @@ testthat::context("cl_cpos2id")
 test_that(
   "cpos2id",
   {
+    registry <- use_tmp_registry()
     ids <- cl_cpos2id(
       corpus = "REUTERS",
       p_attribute = "word",
-      registry = system.file(package = "RcppCWB", "extdata", "cwb", "registry"),
+      registry = registry,
       cpos = 0L:15L
     )
     expect_equal(ids, 0L:15L)
