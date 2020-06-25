5b427e1ab98ab009b1bed6cfd2b01b6b4b90f16c
Andreas Blaette <andreas.blaette@uni-due.de>
2020-06-25 19:31:06 +0200

    write retval

--- src/cwb/cl/storage.c
+++ src/cwb/cl/storage.c
@@ -334,7 +334,7 @@ mmapfile(char *filename, size_t *len_ptr, char *mode)
       /* scroll to the len_ptr'th byte, then overwrite the last integer with a random integer (why? not sure  -- AH),
        * then rewind file */
       lseek(fd, *len_ptr - sizeof(int), SEEK_SET);
-      (void)write(fd, &fd, sizeof(int));
+      ssize_t success = write(fd, &fd, sizeof(int));
       lseek(fd, 0, SEEK_SET);
       
       space = mmap(NULL, *len_ptr, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
