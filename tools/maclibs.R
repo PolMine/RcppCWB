ba1d705399fbaaf7d0ce40cb68c058c47045db44
Andreas Blaette <ablaette@44538.local>
2021-02-02 12:42:36 +0000

    pulling from silicon libglib branch

--- tools/maclibs.R
+++ tools/maclibs.R
@@ -14,9 +14,9 @@ dir.create(target_dir, showWarnings = FALSE)
 untar(target_tarball, exdir = target_dir)
 unlink(target_tarball)
   
-pkgconfig_file <- file.path(target_dir, "libglib-master/pkgconfig/glib-2.0.pc")
+pkgconfig_file <- file.path(target_dir, "libglib-silicon/pkgconfig/glib-2.0.pc")
 pc <- readLines(pkgconfig_file)
-pc[1] <- sprintf("prefix=%s/macos/libglib-master", getwd())
+pc[1] <- sprintf("prefix=%s/macos/libglib-silicon", getwd())
 pc[3] <- sprintf("libdir=${exec_prefix}/lib/%s", Sys.info()[["machine"]])
 writeLines(text = pc, con = pkgconfig_file)
 
