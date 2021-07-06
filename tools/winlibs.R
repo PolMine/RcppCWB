62945227e2088f3c805d3adb973e9335957cd81d
Andreas Blaette <andreas.blaette@uni-due.de>
2021-07-06 15:07:24 +0200

    gcc11 branch of libcl for windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
-if( !file.exists("../windows/libcl-dev/include/cl.h") ){
+if( !file.exists("../windows/libcl-gcc11/include/cl.h") ){
   
   if (getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/PolMine/libcl/archive/dev.zip", "libcl.zip", quiet = TRUE)
+  download.file("https://github.com/PolMine/libcl/archive/gcc11.zip", "libcl.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("libcl.zip", exdir = "../windows")
   unlink("libcl.zip")
