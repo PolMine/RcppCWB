5967a87515f80d6ae68ad4c5f247bab617fb5cdd
Andreas Blaette <andreas.blaette@uni-due.de>
2021-07-10 22:24:20 +0200

    bitfields.c Rprintf header

--- prep/patch.R
+++ prep/patch.R
@@ -49,6 +49,7 @@ for (i in 1L:length(insert_before)){
 
 insert_after <- list(
   "src/cwb/cl/bitio.c" = list("^static\\sint\\sBaseTypeBits", "void Rprintf(const char *, ...);"),
+  "src/cwb/cl/bitfields.c" = list("^static\\sint\\sBaseTypeBits", "void Rprintf(const char *, ...);"),
   "src/cwb/cl/cdaccess.c" = list('^#include\\s"cdaccess\\.h"', "void Rprintf(const char *, ...);")
 )
 
