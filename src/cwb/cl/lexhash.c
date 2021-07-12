5390823a0bca9918c06b80cab7842eb8b79314a5
Andreas Blaette <andreas.blaette@uni-due.de>
2021-07-12 13:39:55 +0200

    patch cl/lexhash.c #2

--- src/cwb/cl/lexhash.c
+++ src/cwb/cl/lexhash.c
@@ -159,6 +159,7 @@ cl_lexhash
 cl_new_lexhash(int buckets)
 {
   cl_lexhash hash;
+  
   if (buckets <= 0)
     buckets = DEFAULT_NR_OF_BUCKETS;
   hash = (cl_lexhash) cl_malloc(sizeof(struct _cl_lexhash));
