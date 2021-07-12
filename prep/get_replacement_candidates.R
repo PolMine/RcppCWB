64b4b4e9565b2aa8fee2912bde8c8fb792327e07
Andreas Blaette <andreas.blaette@uni-due.de>
2021-07-12 17:28:30 +0200

    patching cqp/eval.c #1

--- prep/get_replacement_candidates.R
+++ prep/get_replacement_candidates.R
@@ -2,18 +2,20 @@ diffreport <- readLines("~/Lab/github/RcppCWB/prep/gitdiff_cqp.txt")
 
 diffs <- split(
   diffreport,
-  cut(1:length(x), breaks = grep("^diff\\s--git", x), include.lowest = TRUE, right = FALSE)
+  cut(1:length(diffreport), breaks = grep("^diff\\s--git", diffreport), include.lowest = TRUE, right = FALSE)
 )
 names(diffs) <- unname(sapply(diffs, function(x) gsub("^diff\\s--git\\s+a/(.*?)\\s.*?$", "\\1", x[1])))
 
 y <- lapply(
-  diffs,
-  function(diff){
-    candidates <- grep("^+", diff)
+  1L:length(diffs),
+  function(d){
+    diff <- diffs[[d]]
+    candidates <- grep("^\\-", diff)
     for (i in rev(candidates)){
-      if (!grepl("^-", diff[i + 1L])) candidates[-which(candidates == i)]
+      if (!grepl("^\\+", diff[i + 1L])) candidates <- candidates[-which(candidates == i)]
     }
-    lapply(candidates, function(i) c(diff[i], diff[i + 1L]))
+    lapply(candidates, function(i) sprintf('"%s" = list("^(\\\\s*)%s", "\\\\1%s", 1),', names(diffs)[d], gsub("^\\-\\s*", "", diff[i]), gsub("^\\+\\s*", "", diff[i + 1L] )))
   }
 )
 
+cat(paste(unlist(y), collapse = "\n"))
