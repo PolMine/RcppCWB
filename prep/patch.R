e7bbeacc836909cdc277debb376d548909b344cb
Andreas Blaette <andreas.blaette@uni-due.de>
2021-07-13 17:13:21 +0200

    vfprintf solved

--- prep/patch.R
+++ prep/patch.R
@@ -6,7 +6,6 @@ repodir <- "~/Lab/github/RcppCWB"
 cwb_pkg_dir <- "~/Lab/github/RcppCWB/src/cwb"
 
 global_replacements <- list(
-#  c("vfprintf\\s*\\(\\s*(stderr|stream|stdout|outfd|fd|File|rd->stream|redir->stream),\\s*", "Rprintf("),
   c("(vf|f|v)printf\\s*\\(\\s*(stderr|stream|stdout|outfd|fd|File|rd->stream|redir->stream),\\s*", "Rprintf("),
   c("YY(F|D)PRINTF\\s*\\(\\s*(stderr|yyoutput)," , "YY\\1PRINTF ("),
   c("fprintf\\(", "Rprintf("),
