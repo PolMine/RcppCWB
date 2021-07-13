8baaafe332561ddbc5683b9d87eb31df69a22305
Andreas Blaette <andreas.blaette@uni-due.de>
2021-07-13 13:37:35 +0200

    patching cqp/ranges.c #5

--- prep/patch.R
+++ prep/patch.R
@@ -119,7 +119,8 @@ insert_after <- list(
   "src/cwb/cqp/hash.c" = list("^\\s*\\}\\s*$", "*/", 1),
   "src/cwb/cqp/hash.c" = list("^\\s*\\}\\s*$", "*/", 2),
   "src/cwb/cqp/hash.c" = list("^\\s*\\}\\s*$", "*/", 3),
-  "src/cwb/cqp/output.c" = list("^\\}\\s*$", "*/", 3)
+  "src/cwb/cqp/output.c" = list("^\\}\\s*$", "*/", 3),
+  "src/cwb/cqp/output.c" = list("^}$", "*/", 11L)
 )
 
 for (i in 1L:length(insert_after)){
