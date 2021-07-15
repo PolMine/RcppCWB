8e301c74b195b186b79ac95c7473ca6fad8626cf
Andreas Blaette <andreas.blaette@uni-due.de>
2021-07-15 15:35:10 +0200

    bisoned/flexed files finished

--- prep/patch.R
+++ prep/patch.R
@@ -19,7 +19,7 @@ global_replacements <- list(
   c("YY(F|D)PRINTF\\s*(\\({1,2})\\s*(stderr|yyoutput),\\s*" , "YY\\1PRINTF \\2"),
   c("fprintf\\s*\\(", "Rprintf("),
   c("(\\s+)printf\\(", "\\1Rprintf("),
-  c("#(\\s*)define\\sYYFPRINTF\\sfprintf", "#\\sdefine YYFPRINTF Rprintf"),
+  c("#(\\s*)define\\sYYFPRINTF\\sfprintf", "#\\1define YYFPRINTF Rprintf"),
   
   c('^\\s*#include\\s+"endian\\.h"\\s*$', '#include "endian2.h"') # only files in cl, maybe limit this
 )
