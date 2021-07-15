b553c60284f29b3f2f2c3d17a411697fba96a4e4
Andreas Blaette <andreas.blaette@uni-due.de>
2021-07-15 09:20:35 +0200

    patch cl/lex.creg.c #4

--- prep/patch.R
+++ prep/patch.R
@@ -238,7 +238,7 @@ replace <- list(
   "src/cwb/cqp/regex2dfa.c" = list("^(\\s*)int\\signore_value;", "\\1int ignore_value __attribute__((unused));", 1),
   "src/cwb/cqp/sgml-print.c" = list("^(\\s*)AttributeList\\s\\*strucs;", "\\1/* AttributeList *strucs; */", 1),
   "src/cwb/cqp/sgml-print.c" = list("^(\\s*)strucs\\s=\\scd->printStructureTags;", "\\1/* strucs = cd->printStructureTags; */", 1),
-  "src/cwb/cl/lex.creg.c" = list("^\\s*(static\\svoid\\syyunput\\s\\(int\\sc,char\\s\\*buf_ptr\\s\\);)", "\\1/* \\2 */", 1)
+  "src/cwb/cl/lex.creg.c" = list("^\\s*(static\\svoid\\syyunput\\s\\(int\\sc,char\\s\\*buf_ptr\\s+\\);)", "\\1/* \\2 */", 1)
 )
 
 for (i in 1L:length(replace)){
