47496b90accb93423a0de2ccb56fcc9841823505
Andreas Blaette <andreas.blaette@uni-due.de>
2021-07-15 10:09:43 +0200

    patch cl/lex.creg.c #18

--- prep/patch.R
+++ prep/patch.R
@@ -246,8 +246,8 @@ replace <- list(
   "src/cwb/cqp/sgml-print.c" = list("^(\\s*)strucs\\s=\\scd->printStructureTags;", "\\1/* strucs = cd->printStructureTags; */", 1),
   "src/cwb/cl/lex.creg.c" = list("^(\\s*)(static\\svoid\\syyunput\\s\\(int\\sc,char\\s\\*buf_ptr\\s+\\);).*?$", "\\1/* \\2 */", 1),
   "src/cwb/cl/lex.creg.c" = list("^\\s*static\\svoid\\syyunput\\s\\(int\\sc,\\sregister\\schar\\s\\*\\syy_bp\\s\\)", "static void yyunput (int c, register char * yy_bp )", 1L),
-  "src/cwb/cl/lex.creg.c" = list("^(\\s)\\{(\\s*)/\\*\\sneed\\smore\\sinput\\s\\*/", "\\1{\\2", 1L),
-  "src/cwb/cl/lex.creg.c" = list("^\\s*/\\*\\sThis\\swas\\sreally\\sa\\sNUL\\.\\s\\*/", 1L)
+  "src/cwb/cl/lex.creg.c" = list("^(\\s)\\{(\\s*)/\\*\\sneed\\smore\\sinput\\s\\*/", "\\1{\\2", 1L)
+#  "src/cwb/cl/lex.creg.c" = list("^\\s*/\\*\\sThis\\swas\\sreally\\sa\\sNUL\\.\\s\\*/", 1L)
 )
 
 for (i in 1L:length(replace)){
