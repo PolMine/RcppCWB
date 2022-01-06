f7c4f7b04e3eee63599d379b8499706b618d8069
Andreas Blaette <andreas.blaette@uni-due.de>
2022-01-06 18:19:57 +0100

    message what action & debugging

--- prep/autopatch/R/patch.R
+++ prep/autopatch/R/patch.R
@@ -71,7 +71,6 @@ patch_functions <- list(
       sprintf("regex: %s | match: %d | replacement: %s ... ", action[[1]], action[[3]], paste(action[[2]], collapse = "///")),
       appendLF = FALSE
     )
-    code <- readLines(fname)
     position <- grep(pattern = action[[1]], code)[ action[[3]] ]
     if (!is.na(position)){
       code[position] <- gsub(action[1], action[2], code[position])
@@ -123,6 +122,7 @@ patch_file <- function(file, actions){
   } else {
     code <- readLines(file)
     for (i in 1L:length(actions)){
+      message("Action to perform: ", names(actions)[i])
       code <- patch_functions[[ names(actions)[i] ]](code = code, action = actions[[i]])
     }
     writeLines(code, file)
