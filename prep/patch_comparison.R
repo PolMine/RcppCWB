41da22a607262bfd76445d86a03d7189581319b4
Andreas Blaette <andreas.blaette@uni-due.de>
2022-01-07 15:09:49 +0100

    comparing patches

--- prep/patch_comparison.R
+++ prep/patch_comparison.R
@@ -9,23 +9,34 @@ source("~/Lab/github/RcppCWB/prep/autopatch/R/PatchEngine.R")
 data_files <- list.files("~/Lab/github/RcppCWB/prep/autopatch/data", full.names = TRUE)
 for (fname in data_files) source(fname)
 
-PE <- lapply(
-  c(1069, 1071),
-  function(revision){
-    PatchEngine$new(
+
+
+PE <- list()
+
+PE[["1069"]] <- PatchEngine$new(
       cwb_dir_svn = "~/Lab/tmp/cwb/trunk",
       repodir = "~/Lab/github/RcppCWB",
-      revision = revision, # see explanation below
+      revision = 1069,
       global_replacements = global_replacements,
       file_patches = file_patches
-    )$patch_all()
-    
-  }
 )
+PE[["1069"]]$patch_all()
+    
+update <- "1100"
+PE[[update]] <- PatchEngine$new(
+  cwb_dir_svn = "~/Lab/tmp/cwb/trunk",
+  repodir = "~/Lab/github/RcppCWB",
+  revision = as.integer(update),
+  global_replacements = global_replacements,
+  file_patches = file_patches
+)
+PE[[update]]$patch_all()
 
-names(P_1069$diff_file_patches) %in% names(P_1071$diff_file_patches)
+
+
+names(PE[[1]]$diff_file_patches) %in% names(PE[[2]]$diff_file_patches)
 
 sapply(
-  names(P_1069$diff_file_patches),
-  function(x) identical(P_1069$diff_file_patches[[x]], P_1071$diff_file_patches[[x]])
+  names(PE[[1]]$diff_file_patches),
+  function(x) identical(PE[[1]]$diff_file_patches[[x]], PE[[2]]$diff_file_patches[[x]])
 )
