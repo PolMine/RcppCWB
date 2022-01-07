3786a957d0d419bafee054aabc9e235931f0b7c3
Andreas Blaette <andreas.blaette@uni-due.de>
2022-01-07 14:57:47 +0100

    patch_comparison.R fetched

--- prep/patch_comparison.R
+++ prep/patch_comparison.R
@@ -3,26 +3,29 @@ library(fs)
 library(magrittr)
 library(R6)
 
+
 source("~/Lab/github/RcppCWB/prep/autopatch/R/PatchEngine.R")
 
 data_files <- list.files("~/Lab/github/RcppCWB/prep/autopatch/data", full.names = TRUE)
 for (fname in data_files) source(fname)
 
-P_1069 <- PatchEngine$new(
-  cwb_dir_svn = "~/Lab/tmp/cwb/trunk",
-  repodir = "~/Lab/github/RcppCWB",
-  revision = 1069, # see explanation below
-  global_replacements = global_replacements,
-  file_patches = file_patches
+PE <- lapply(
+  c(1069, 1071),
+  function(revision){
+    PatchEngine$new(
+      cwb_dir_svn = "~/Lab/tmp/cwb/trunk",
+      repodir = "~/Lab/github/RcppCWB",
+      revision = revision, # see explanation below
+      global_replacements = global_replacements,
+      file_patches = file_patches
+    )$patch_all()
+    
+  }
 )
-P_1069$patch_all()
 
+names(P_1069$diff_file_patches) %in% names(P_1071$diff_file_patches)
 
-P_1071 <- PatchEngine$new(
-  cwb_dir_svn = "~/Lab/tmp/cwb/trunk",
-  repodir = "~/Lab/github/RcppCWB",
-  revision = 1071, # see explanation below
-  global_replacements = global_replacements,
-  file_patches = file_patches
+sapply(
+  names(P_1069$diff_file_patches),
+  function(x) identical(P_1069$diff_file_patches[[x]], P_1071$diff_file_patches[[x]])
 )
-P_1071$patch_all()
