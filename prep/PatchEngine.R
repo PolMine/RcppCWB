01daf9ab5a925a6c889fff5204c70bee135d901a
Andreas Blaette <andreas.blaette@uni-due.de>
2022-01-09 00:12:49 +0100

    debug msg

--- prep/PatchEngine.R
+++ prep/PatchEngine.R
@@ -298,6 +298,7 @@ PatchEngine <- R6Class(
     extern = function(code, action, file){
       if (length(action) == 0L) return(code)
       for (ext in action){
+        print(ext)
         matches <- which(startsWith(code, ext))
         if (length(matches) > 0L){
           for (position in matches){
