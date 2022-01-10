e8b01e514c713d657055fb301cfc7db7bc64c8e0
Andreas Blaette <andreas.blaette@uni-due.de>
2022-01-10 22:02:28 +0100

    adjust blank lines #12

--- prep/PatchEngine.R
+++ prep/PatchEngine.R
@@ -1256,7 +1256,7 @@ PatchEngine <- R6Class(
           replace = list("register\\s+", "", NA),
           
           delete_line_before = list("^encode_usage\\(void\\)\\s*$", 1L, 4L),
-          delete_line_beginning_with = list("^encode_usage\\(void\\)\\s*$", 1L, 70L),
+          delete_line_beginning_with = list("^encode_usage\\(void\\)\\s*$", 1L, 72L),
           
           delete_line_before = list("^encode_parse_options\\(int\\sargc,\\schar\\s\\*\\*argv\\)\\s*$", 1L, 10L),
           delete_line_beginning_with = list("^encode_parse_options\\(int\\sargc,\\schar\\s\\*\\*argv\\)\\s*$", 1L, 237L),
