e133bdb555089ea10a3a2ca43977d0e13e4590a5
Andreas Blaette <andreas.blaette@uni-due.de>
2022-01-15 01:35:20 +0100

    extern

--- prep/PatchEngine.R
+++ prep/PatchEngine.R
@@ -1352,6 +1352,7 @@ PatchEngine <- R6Class(
           replace = list("^(\\s*)int\\srange_ptr\\s*=.*?;", "\\1extern int range_ptr;", 1L),
           replace = list("^(\\s*)SAttEncoder\\sranges\\[MAXRANGES\\];", "extern SAttEncoder ranges[MAXRANGES];", 1L),
           replace = list("^(\\s*)cl_lexhash\\sundeclared_sattrs\\s*=.*?;", "\\1extern cl_lexhash undeclared_sattrs;", 1L),
+          replace = list("^(\\s*)int\\swattr_ptr\\s=\\s0;", "\\1extern wattr_ptr;", 1L),
           
           delete_line_beginning_with = list("^/\\*\\*\\sname\\sof\\sthe\\scurrently\\srunning\\sprogram", 1L, 1L),
           
@@ -1622,7 +1623,7 @@ PatchEngine <- R6Class(
       h <- c(
         "/* Do not edit by hand! This file has been automatically generated */",
         "",
-        "Corpus *corpus;",
+        "extern Corpus *corpus;",
         "typedef struct _SAttEncoder SAttEncoder;",
         "", "",
         unlist(h_li)
