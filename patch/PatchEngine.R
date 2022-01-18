22bb3ba2653188f0d1171278cd4efe8441b3e5f7
Andreas Blaette <andreas.blaette@uni-due.de>
2022-01-18 15:30:31 +0100

    delete->del

--- patch/PatchEngine.R
+++ patch/PatchEngine.R
@@ -1197,7 +1197,22 @@ PatchEngine <- R6Class(
         ),
         
         "src/cwb/cqp/parse_actions.c" = c(
-          list(
+          if (revision >= 1690) list(
+            replace = list("^(\\s*)c->idlist\\.delete\\s=\\sleft->pa_ref\\.delete;", "\\1c->idlist.del = left->pa_ref.del;", 1),
+            
+            replace = list("^(\\s*)left->pa_ref\\.delete\\s=\\s0;", "\\1left->pa_ref.del = 0;", 1),
+            replace = list("^(\\s*)node->idlist\\.delete\\s=\\s0;", "\\1node->idlist.del = 0;", 1),
+            
+            replace = list("^(\\s*)result->idlist\\.delete\\s=\\sleft->pa_ref\\.delete;", "\\1resut->idlist.del = left->pa_ref.del;", 1),
+            replace = list("^(\\s*)result->pa_ref\\.delete\\s=\\sauto_delete;", "\\1result->pa_ref.del = auto_delete;", 1),
+            replace = list("^(\\s*)result->sa_ref\\.delete\\s=\\sauto_delete;", "\\1result->sa_ref.del = auto_delete;", 1),
+            
+            replace = list("^(\\s*)result->pa_ref\\.delete\\s=\\s0;", "\\1result->pa_ref.del = 0;", 1),
+            replace = list("^(\\s*)result->pa_ref\\.delete\\s=\\sauto_delete;", "\\1result->pa_ref.del = auto_delete;", 1),
+            replace = list("^(\\s*)result->sa_ref\\.delete\\s=\\s0;", "\\1result->sa_ref.del = 0;", 1)
+            
+          ),
+          if (revision == 1069) list(
             replace = list("^(\\s*)c->idlist\\.delete\\s=\\sleft->pa_ref\\.delete;", "\\1c->idlist.del = left->pa_ref.del;", 1),
             replace = list("^(\\s*)left->pa_ref\\.delete\\s=\\s0;", "\\1left->pa_ref.del = 0;", 1),
             replace = list("^(\\s*)node->idlist\\.delete\\s=\\s0;", "\\1node->idlist.del = 0;", 1),
@@ -1207,8 +1222,6 @@ PatchEngine <- R6Class(
             replace = list("^(\\s*)res->pa_ref\\.delete\\s=\\s0;", "\\1res->pa_ref.del = 0;", 1),
             replace = list("^(\\s*)res->pa_ref\\.delete\\s=\\sauto_delete;", "\\1res->pa_ref.del = auto_delete;", 1),
             replace = list("^(\\s*)res->sa_ref\\.delete\\s=\\s0;", "\\1res->sa_ref.del = 0;", 1)
-          ),
-          if (revision == 1069) list(
           )
         ),
         
