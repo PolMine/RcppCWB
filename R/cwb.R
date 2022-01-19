b8d7bd84303f3b5678d0956b183ecb10450c069a
Andreas Blaette <andreas.blaette@uni-due.de>
2022-01-19 15:47:24 +0100

    cwb_version() returns character vector

--- R/cwb.R
+++ R/cwb.R
@@ -165,8 +165,7 @@ cwb_encode <- function(corpus, registry = Sys.getenv("CORPUS_REGISTRY"), data_di
 #' Get the CWB version used and available when compiling the source code.
 #' 
 #' @export
-#' @return A `numeric_version` vector.
+#' @return A length-one `character` vector.
 #' @examples
 #' cwb_version()
-#' cwb_version() > numeric_version("3.4.14")
-cwb_version <- function() numeric_version(.cwb_version())
+cwb_version <- function() .cwb_version()
