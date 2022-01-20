ec74cdd5b1f686c4b85b57da0eb2f871587b0754
Andreas Blaette <andreas.blaette@uni-due.de>
2022-01-20 21:57:46 +0100

    one single def of CWB version

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,9 +1,11 @@
-if( !file.exists("../windows/libcl-v3.4.33/include/cl.h") ){
+version <- commandArgs(TRUE)
+
+if ( !file.exists(sprintf("../windows/libcl-%s/include/cl.h", version))){
   
   if (getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/PolMine/libcl/archive/v3.4.33.zip", "v3.4.33.zip", quiet = TRUE)
+  download.file(sprintf("https://github.com/PolMine/libcl/archive/v%s.zip", version), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
-  unzip("v3.4.33.zip", exdir = "../windows")
-  unlink("v3.4.33.zip")
+  unzip("lib.zip", exdir = "../windows")
+  unlink("lib.zip")
   
 }
