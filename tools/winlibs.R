273c9dc6867937b2108194fb0a912c157a254b1f
Andreas Blaette <andreas.blaette@uni-due.de>
2022-01-20 08:57:09 +0100

    get winlibs from libcl branch with version number

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,9 +1,9 @@
-if( !file.exists("../windows/libcl-gcc11/include/cl.h") ){
+if( !file.exists("../windows/libcl-v3.4.33/include/cl.h") ){
   
   if (getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/PolMine/libcl/archive/patch.zip", "libcl.zip", quiet = TRUE)
+  download.file("https://github.com/PolMine/libcl/archive/v3.4.33.zip", "v3.4.33.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
-  unzip("libcl.zip", exdir = "../windows")
-  unlink("libcl.zip")
+  unzip("v3.4.33.zip", exdir = "../windows")
+  unlink("v3.4.33.zip")
   
 }
