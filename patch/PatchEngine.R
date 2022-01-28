78e004ad304f856e7522dbdbcb456fe9b49f9881
Andreas Blaette <andreas.blaette@uni-due.de>
2022-01-28 14:24:09 +0100

    deal with pointer sign issues #5

--- patch/PatchEngine.R
+++ patch/PatchEngine.R
@@ -1365,7 +1365,7 @@ PatchEngine <- R6Class(
             # 1027 |   WINSOCK_API_LINKAGE int WSAAPI send(SOCKET s,const char *buf,int len,int flags);
             # |                                                ~~~~~~~~~~~~^~~
             #   
-            replace = list("^\\s*prep\\s=\\s\\(unsigned\\schar\\)\\s0xff\\s&\\sn;$", "prep = (const char) 0xff & n;)", 1L)
+            replace = list("^(\\s*)prep\\s=\\s\\(unsigned\\schar\\)\\s0xff\\s&\\sn;$", "\\1prep = (const char) 0xff & n;)", 1L)
           ),
           
           # File 'lexhash.h' not there any more at r1690
