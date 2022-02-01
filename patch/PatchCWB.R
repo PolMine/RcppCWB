3d9a257b288c955162e3c26c28ecbc5e95e35b75
Andreas Blaette <andreas.blaette@uni-due.de>
2022-02-01 23:45:34 +0100

    cwb-huffcode robust #3

--- patch/PatchCWB.R
+++ patch/PatchCWB.R
@@ -1790,7 +1790,7 @@ PatchCWB <- R6Class(
             replace = list("^\\s*NwriteInt\\(pos,\\ssync\\);\\s*$", "word = htonl(pos);", 1L),
             insert_after = list(
               "^\\s*word\\s=\\shtonl\\(pos\\);\\s*$",
-              c("success = fwrite(&word, sizeof(int), 1, sync);", 'if (success != 1) Rprintf("File write error!\n");'),
+              c("success = fwrite(&word, sizeof(int), 1, sync);", 'if (success != 1) Rprintf("File write error!\\n");'),
               1L
             )
             
