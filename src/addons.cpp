aad44f4ea595c83b62b5752af76fecd5492152fd
Andreas Blaette <andreas.blaette@uni-due.de>
2022-02-27 20:30:11 +0100

    .region_matrix_context() implements boundary and s_attr

--- src/addons.cpp
+++ src/addons.cpp
@@ -4,6 +4,7 @@ extern "C" {
   #include <unistd.h>
   #include <string.h>
   #include "cl.h"
+  #include "cwb/cl/cwb-globals.h" 
   #include <pcre.h>
 }
 
@@ -180,54 +181,170 @@ Rcpp::IntegerVector region_matrix_to_count_matrix(SEXP corpus, SEXP p_attribute,
 // [[Rcpp::export(name=".region_matrix_context")]]
 Rcpp::IntegerMatrix region_matrix_context(SEXP corpus, SEXP registry, Rcpp::IntegerMatrix region_matrix, SEXP s_attribute, SEXP boundary, int left, int right){
   
-  int i, size, struc_node, struc_left, struc_right, lb, rb;
-  int struc_boundary_node, lb_boundary, rb_boundary;
-
-  Attribute* s_attr = make_s_attribute(corpus, s_attribute, registry);
-  Attribute* limit = make_s_attribute(corpus, boundary, registry);
-  size = cl_max_struc(s_attr);
-
+  int i, size;
+  int lb, rb;
   Rcpp::IntegerMatrix context_matrix(region_matrix.nrow(), 2);
 
-  for (i = 0; i < region_matrix.nrow(); i++){
+  if (boundary == R_NilValue){
     
-    struc_node = cl_cpos2struc(s_attr, region_matrix(i, 0));
-    struc_boundary_node = cl_cpos2struc(limit, region_matrix(i, 0));
-
-    struc_left = struc_node - left;
-    if (struc_left >= 0){
+    if (s_attribute == R_NilValue){
       
-      cl_struc2cpos(s_attr, struc_left, &lb, &rb);
-      cl_struc2cpos(limit, struc_boundary_node, &lb_boundary, &rb_boundary);
+      SEXP default_p_attr = Rf_mkChar(CWB_DEFAULT_ATT_NAME);
+      Attribute* p_attr = make_p_attribute(corpus, default_p_attr, registry);
+      size = cl_max_cpos(p_attr);
+      int cpos_left, cpos_right;
       
-      if (lb_boundary == region_matrix(i,0)){
-        context_matrix(i,0) = NA_INTEGER;
-      } else if (lb_boundary > lb){
-        context_matrix(i,0) = lb_boundary;
-      } else {
-        context_matrix(i,0) = lb;
+      for (i = 0; i < region_matrix.nrow(); i++){
+        
+        cpos_left = region_matrix(i,0) - left;
+        if (cpos_left >= 0){
+          context_matrix(i,0) = cpos_left;
+        } else {
+          context_matrix(i,0) = 0;
+        }
+        
+        cpos_right = region_matrix(i,1) + right;
+        if (cpos_right < size){
+          context_matrix(i,1) = cpos_right;
+        } else {
+          context_matrix(i,1) = size - 1;
+        }
       }
+      
     } else {
-      context_matrix(i,0) = NA_INTEGER;
+      
+      Attribute* s_attr = make_s_attribute(corpus, s_attribute, registry);
+      size = cl_max_struc(s_attr);
+      int struc_node, struc_left, struc_right;
+      
+      for (i = 0; i < region_matrix.nrow(); i++){
+        
+        struc_node = cl_cpos2struc(s_attr, region_matrix(i, 0));
+
+        struc_left = struc_node - left;
+        if (struc_left >= 0){
+          
+          cl_struc2cpos(s_attr, struc_left, &lb, &rb);
+
+          if (lb == region_matrix(i,0)){
+            context_matrix(i,0) = NA_INTEGER;
+          } else {
+            context_matrix(i,0) = lb;
+          }
+        } else {
+          context_matrix(i,0) = NA_INTEGER;
+        }
+        
+        struc_right = struc_node + right;
+        if (struc_right <= size){
+          
+          cl_struc2cpos(s_attr, struc_right, &lb, &rb);
+
+          if (rb == region_matrix(i,1)){
+            context_matrix(i,1) = NA_INTEGER;
+          } else {
+            context_matrix(i,1) = rb;
+          }
+        } else {
+          context_matrix(i,3) = NA_INTEGER;
+        }
+      }
+      
     }
     
-    struc_right = struc_node + right;
-    if (struc_right <= size){
-      
-      cl_struc2cpos(s_attr, struc_right, &lb, &rb);
-      cl_struc2cpos(limit, struc_boundary_node, &lb_boundary, &rb_boundary);
-      
-      if (rb_boundary == region_matrix(i,1)){
-        context_matrix(i,1) = NA_INTEGER;
-      } else if (rb_boundary < rb){
-        context_matrix(i,1) = rb_boundary;
-      } else {
-        context_matrix(i,1) = rb;
-      }
-    } else {
-      context_matrix(i,3) = NA_INTEGER;
-    }
+  } else {
+    
+    int struc_boundary_node, lb_boundary, rb_boundary;
+    Attribute* limit = make_s_attribute(corpus, boundary, registry);
 
+    if (s_attribute == R_NilValue){
+      
+      SEXP default_p_attr = Rf_mkChar(CWB_DEFAULT_ATT_NAME);
+      Attribute* p_attr = make_p_attribute(corpus, default_p_attr, registry);
+      size = cl_max_cpos(p_attr);
+      int cpos_left, cpos_right;
+      
+      for (i = 0; i < region_matrix.nrow(); i++){
+        
+        cpos_left = region_matrix(i,0) - left;
+        
+        struc_boundary_node = cl_cpos2struc(limit, region_matrix(i, 0));
+        if (cpos_left >= 0){
+          cl_struc2cpos(limit, struc_boundary_node, &lb_boundary, &rb_boundary);
+          if (lb_boundary == region_matrix(i,0)){
+            context_matrix(i,0) = NA_INTEGER;
+          } else if (lb_boundary > cpos_left){
+            context_matrix(i,0) = lb_boundary;
+          } else {
+            context_matrix(i,0) = cpos_left;
+          }
+        } else {
+          context_matrix(i,0) = NA_INTEGER;
+        }
+        
+        cpos_right = region_matrix(i,1) + right;
+        if (cpos_right < size){
+          cl_struc2cpos(limit, struc_boundary_node, &lb_boundary, &rb_boundary);
+          if (rb_boundary == region_matrix(i,1)){
+            context_matrix(i,1) = NA_INTEGER;
+          } else if (rb_boundary < cpos_right){
+            context_matrix(i,1) = rb_boundary;
+          } else {
+            context_matrix(i,1) = cpos_right;
+          }
+        } else {
+          context_matrix(i,1) = NA_INTEGER;
+        }
+        
+      }
+      
+    } else {
+      
+      Attribute* s_attr = make_s_attribute(corpus, s_attribute, registry);
+      size = cl_max_struc(s_attr);
+      int struc_node, struc_left, struc_right;
+      
+      for (i = 0; i < region_matrix.nrow(); i++){
+        
+        struc_node = cl_cpos2struc(s_attr, region_matrix(i, 0));
+        struc_boundary_node = cl_cpos2struc(limit, region_matrix(i, 0));
+        
+        struc_left = struc_node - left;
+        if (struc_left >= 0){
+          
+          cl_struc2cpos(s_attr, struc_left, &lb, &rb);
+          cl_struc2cpos(limit, struc_boundary_node, &lb_boundary, &rb_boundary);
+          
+          if (lb_boundary == region_matrix(i,0)){
+            context_matrix(i,0) = NA_INTEGER;
+          } else if (lb_boundary > lb){
+            context_matrix(i,0) = lb_boundary;
+          } else {
+            context_matrix(i,0) = lb;
+          }
+        } else {
+          context_matrix(i,0) = NA_INTEGER;
+        }
+        
+        struc_right = struc_node + right;
+        if (struc_right <= size){
+          
+          cl_struc2cpos(s_attr, struc_right, &lb, &rb);
+          cl_struc2cpos(limit, struc_boundary_node, &lb_boundary, &rb_boundary);
+          
+          if (rb_boundary == region_matrix(i,1)){
+            context_matrix(i,1) = NA_INTEGER;
+          } else if (rb_boundary < rb){
+            context_matrix(i,1) = rb_boundary;
+          } else {
+            context_matrix(i,1) = rb;
+          }
+        } else {
+          context_matrix(i,3) = NA_INTEGER;
+        }
+      }
+    }
   }
+
   return context_matrix;
 }
