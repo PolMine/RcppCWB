a9e392045c212fe285255b0246f94adc2521aa83
Andreas Blätte <andreasblatte@MBP-von-Andreas.fritz.box>
2022-03-25 09:29:36 +0100

    corpus_full_name() drafted

---
+++ tests/testthat/test_registry_info.R
@@ -0,0 +1,23 @@
+library(RcppCWB)
+use_tmp_registry()
+testthat::context("subcorpus")
+
+test_that(
+  "check result of corpus_data_dir()",
+  {
+    expect_true(
+      dir.exists(
+        corpus_data_dir("REUTERS", registry = get_tmp_registry())
+      )
+    )
+  }
+)
+
+test_that(
+  "check result of corpus_info_file()",
+  {
+    f <- corpus_info_file("REUTERS", registry = get_tmp_registry())
+    expect_true(file.exists(f))
+    expect_identical(length(readLines(f)), 8L)
+  }
+)
