59a544e1d73a5f55da7124566a1e75f205b209f8
Andreas Blätte <andreasblatte@MBP-von-Andreas.fritz.box>
2024-02-29 09:52:02 +0100

    documentation updated for cwb_encode() arg s_attributes #90

--- R/cwb.R
+++ R/cwb.R
@@ -5,6 +5,27 @@
 #' will always perform a specific indexing/compression step on one positional
 #' attribute, and produce all components.
 #' 
+#' @param p_attributes Positional attributes (p-attributes) to be declared.
+#' @param data_dir The data directory where `cwb_encode` will save the binary
+#'   files of the indexed corpus.  Tilde expansion is performed on `data_dir`
+#'   using `path.expand()` to avoid a crash.
+#' @param vrt_dir Directory with input corpus files (verticalised format / file
+#'   ending *.vrt). Tilde expansion is performed on `vrt_dir` using
+#'   `path.expand()` to avoid a crash.
+#' @param encoding The encoding of the files to be encoded. Needs to be an
+#'   encoding supported by CWB, see `cwb_charsets()`. "UTF-8" is taken as
+#'   "utf8". Defaults to "utf8" (recommended charset).
+#' @param s_attributes A `list` of named `character` vectors to declare
+#'   structural attributes that shall be encoded. The names of the list are the
+#'   XML elements present in the corpus. Character vectors making up the list
+#'   declare the attributes that include the metadata of regions. To declare a
+#'   structural attribute without annotations, provide a zero-length character
+#'   vector using `character()` - see examples.
+#' @param skip_blank_lines A `logical` value, whether to skip blank lines in the
+#'   input.
+#' @param strip_whitespace A `logical` value, whether to strip whitespace from
+#'   tokens
+#' @param xml A `logical` value, whether input is XML.
 #' @param corpus Name of a CWB corpus (upper case).
 #' @param p_attribute Name of p-attribute.
 #' @param registry Path to the registry directory, defaults to the value of the
@@ -13,6 +34,120 @@
 #'   warnings).
 #' @param verbose A `logical` value, whether to show progress information
 #'   (counter of tokens processed).
+#' @rdname cwb_utils
+#' @export cwb_encode
+#' @importFrom fs path
+#' @examples
+#' data_dir <- file.path(tempdir(), "bt_data_dir")
+#' dir.create(data_dir)
+#' 
+#' cwb_encode(
+#'   corpus = "BTMIN",
+#'   registry = Sys.getenv("CORPUS_REGISTRY"),
+#'   vrt_dir = system.file(package = "RcppCWB", "extdata", "vrt"),
+#'   data_dir = data_dir,
+#'   p_attributes = c("word", "pos", "lemma"),
+#'   s_attributes = list(
+#'     plenary_protocol = c(
+#'       "lp", "protocol_no", "date", "year", "birthday", "version",
+#'       "url", "filetype"
+#'     ),
+#'     speaker = c(
+#'       "id", "type", "lp", "protocol_no", "date", "year", "ai_no", "ai_id",
+#'       "ai_type", "who", "name", "parliamentary_group", "party", "role"
+#'      ),
+#'     p = character()
+#'   )
+#' )
+#' 
+#' unlink(data_dir)
+#' unlink(file.path(Sys.getenv("CORPUS_REGISTRY"), "btmin"))
+cwb_encode <- function(
+    corpus,
+    registry = Sys.getenv("CORPUS_REGISTRY"),
+    data_dir,
+    vrt_dir,
+    encoding = "utf8",
+    p_attributes = c("word", "pos", "lemma"),
+    s_attributes = list(),
+    skip_blank_lines = TRUE,
+    strip_whitespace = TRUE,
+    xml = TRUE,
+    quietly = FALSE, 
+    verbose = FALSE
+){
+  
+  if (encoding == "UTF-8") encoding <- "utf8"
+  if (!encoding %in% cwb_charsets()) stop(
+    sprintf(
+      "encoding '%' is not a valid CWB character set, see cwb_charsets() for options",
+      cwb_charsets
+    )
+  )
+  
+  stopifnot(
+    is.character(corpus), length(corpus) == 1L,
+    is.character(registry), length(registry) == 1L, dir.exists(registry),
+    is.character(data_dir), length(data_dir) == 1L,
+    dir.exists(data_dir), length(list.files(data_dir)) == 0L,
+    is.character(vrt_dir), length(vrt_dir) == 1L, dir.exists(vrt_dir),
+    is.character(encoding), length(encoding) == 1L,
+    is.character(p_attributes),
+    is.logical(skip_blank_lines), length(skip_blank_lines) == 1L,
+    is.logical(strip_whitespace), length(strip_whitespace) == 1L,
+    is.logical(xml), length(xml) == 1L
+  )
+  
+  if (length(list.files(vrt_dir)) < 1L)
+    stop("No files in directory 'vrt_dir'")
+  
+  s_attributes_noanno <- unlist(lapply(
+    names(s_attributes),
+    function(s_attr)
+      if (length(s_attributes[[s_attr]]) == 0L) s_attr else character()
+  ))
+  # zero-length scenario results in NULL
+  if (is.null(s_attributes_noanno)) s_attributes_noanno <- character()
+  
+  for (s_attr in s_attributes_noanno) s_attributes[[s_attr]] <- NULL
+  
+  s_attributes_anno <- unname(
+    unlist(
+      lapply(
+        names(s_attributes),
+        function(s_attr)
+          paste(
+            s_attr, ":", 0L, "+",
+            paste(s_attributes[[s_attr]], collapse = "+"),
+            sep = ""
+          )
+      )
+    )
+  )
+  if (is.null(s_attributes_anno)) s_attributes_anno <- character()
+  
+  # Ensure that paths are standardized
+  regfile <- as.character(fs::path(fs::path_expand(registry), tolower(corpus)))
+  data_dir <- as.character(fs::path(fs::path_expand(data_dir)))
+  vrt_dir <- as.character(fs::path(fs::path_expand(vrt_dir)))
+  
+  .cwb_encode(
+    regfile = regfile,
+    data_dir = data_dir,
+    vrt_dir = vrt_dir,
+    encoding = encoding,
+    p_attributes = p_attributes,
+    s_attributes_anno = s_attributes_anno,
+    s_attributes_noanno = s_attributes_noanno,
+    skip_blank_lines = skip_blank_lines,
+    xml = xml,
+    strip_whitespace = strip_whitespace,
+    quiet = quietly,
+    verbosity = verbose
+  )
+}
+
+
 #' @examples
 #' # The package includes and 'unfinished' corpus of debates in the UN General 
 #' # Assembly ("UNGA"), i.e. it does not yet include the reverse index, and it 
@@ -244,139 +379,6 @@ cwb_compress_rdx <- function(
   success
 }
 
-#' @param p_attributes Positional attributes (p-attributes) to be declared.
-#' @param data_dir The data directory where `cwb_encode` will save the binary
-#'   files of the indexed corpus.  Tilde expansion is performed on `data_dir`
-#'   using `path.expand()` to avoid a crash.
-#' @param vrt_dir Directory with input corpus files (verticalised format / file
-#'   ending *.vrt). Tilde expansion is performed on `vrt_dir` using
-#'   `path.expand()` to avoid a crash.
-#' @param encoding The encoding of the files to be encoded. Needs to be an
-#'   encoding supported by CWB, see `cwb_charsets()`. "UTF-8" is taken as
-#'   "utf8". Defaults to "utf8" (recommended charset).
-#' @param s_attributes A `list` of named `character` vectors to declare
-#'   structural attributes that shall be encoded. The names of the list are the
-#'   XML elements present in the corpus. Character vectors making up the list
-#'   declare the attributes that include the metadata of regions. To declare a
-#'   structural attribute without annotations, provide a zero-length character
-#'   vector using `character()` - see examples.
-#' @param skip_blank_lines A `logical` value, whether to skip blank lines in the
-#'   input.
-#' @param strip_whitespace A `logical` value, whether to strip whitespace from
-#'   tokens
-#' @param xml A `logical` value, whether input is XML.
-#' @rdname cwb_utils
-#' @export cwb_encode
-#' @importFrom fs path
-#' @examples
-#' data_dir <- file.path(tempdir(), "bt_data_dir")
-#' dir.create(data_dir)
-#' 
-#' cwb_encode(
-#'   corpus = "BTMIN",
-#'   registry = Sys.getenv("CORPUS_REGISTRY"),
-#'   vrt_dir = system.file(package = "RcppCWB", "extdata", "vrt"),
-#'   data_dir = data_dir,
-#'   p_attributes = c("word", "pos", "lemma"),
-#'   s_attributes = list(
-#'     plenary_protocol = c(
-#'       "lp", "protocol_no", "date", "year", "birthday", "version",
-#'       "url", "filetype"
-#'     ),
-#'     speaker = c(
-#'       "id", "type", "lp", "protocol_no", "date", "year", "ai_no", "ai_id",
-#'       "ai_type", "who", "name", "parliamentary_group", "party", "role"
-#'      ),
-#'     p = character()
-#'   )
-#' )
-#' 
-#' unlink(data_dir)
-#' unlink(file.path(Sys.getenv("CORPUS_REGISTRY"), "btmin"))
-cwb_encode <- function(
-  corpus,
-  registry = Sys.getenv("CORPUS_REGISTRY"),
-  data_dir,
-  vrt_dir,
-  encoding = "utf8",
-  p_attributes = c("word", "pos", "lemma"),
-  s_attributes = list(),
-  skip_blank_lines = TRUE,
-  strip_whitespace = TRUE,
-  xml = TRUE,
-  quietly = FALSE, 
-  verbose = FALSE
-){
-  
-  if (encoding == "UTF-8") encoding <- "utf8"
-  if (!encoding %in% cwb_charsets()) stop(
-    sprintf(
-      "encoding '%' is not a valid CWB character set, see cwb_charsets() for options",
-      cwb_charsets
-    )
-  )
-  
-  stopifnot(
-    is.character(corpus), length(corpus) == 1L,
-    is.character(registry), length(registry) == 1L, dir.exists(registry),
-    is.character(data_dir), length(data_dir) == 1L,
-    dir.exists(data_dir), length(list.files(data_dir)) == 0L,
-    is.character(vrt_dir), length(vrt_dir) == 1L, dir.exists(vrt_dir),
-    is.character(encoding), length(encoding) == 1L,
-    is.character(p_attributes),
-    is.logical(skip_blank_lines), length(skip_blank_lines) == 1L,
-    is.logical(strip_whitespace), length(strip_whitespace) == 1L,
-    is.logical(xml), length(xml) == 1L
-  )
-  
-  if (length(list.files(vrt_dir)) < 1L)
-    stop("No files in directory 'vrt_dir'")
-  
-  s_attributes_noanno <- unlist(lapply(
-    names(s_attributes),
-    function(s_attr)
-      if (length(s_attributes[[s_attr]]) == 0L) s_attr else character()
-  ))
-  # zero-length scenario results in NULL
-  if (is.null(s_attributes_noanno)) s_attributes_noanno <- character()
-  
-  for (s_attr in s_attributes_noanno) s_attributes[[s_attr]] <- NULL
-  
-  s_attributes_anno <- unname(
-    unlist(
-      lapply(
-        names(s_attributes),
-        function(s_attr)
-          paste(
-            s_attr, ":", 0L, "+",
-            paste(s_attributes[[s_attr]], collapse = "+"),
-            sep = ""
-          )
-      )
-    )
-  )
-  if (is.null(s_attributes_anno)) s_attributes_anno <- character()
-  
-  # Ensure that paths are standardized
-  regfile <- as.character(fs::path(fs::path_expand(registry), tolower(corpus)))
-  data_dir <- as.character(fs::path(fs::path_expand(data_dir)))
-  vrt_dir <- as.character(fs::path(fs::path_expand(vrt_dir)))
-  
-  .cwb_encode(
-    regfile = regfile,
-    data_dir = data_dir,
-    vrt_dir = vrt_dir,
-    encoding = encoding,
-    p_attributes = p_attributes,
-    s_attributes_anno = s_attributes_anno,
-    s_attributes_noanno = s_attributes_noanno,
-    skip_blank_lines = skip_blank_lines,
-    xml = xml,
-    strip_whitespace = strip_whitespace,
-    quiet = quietly,
-    verbosity = verbose
-  )
-}
 
 #' Get CWB version
 #' 
